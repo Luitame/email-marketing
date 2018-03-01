2c62e004629d6c2b73ed315e2ebaf47391aa72f6
Luítame de Oliveira <luitamy@hotmail.com>
2018-03-01 18:04:14 -0300

    start using mongodb

---
+++ src/db/connection.js
@@ -0,0 +1,12 @@
+let mongoose = require('mongoose');
+
+mongoose.connect(
+    'mongodb://mongodb:27017/email_marketing', { useMongoClient: true, function (err) {
+        if(err) {
+            console.log('Mongoose error => '. err);
+        }
+
+        console.log('Mongoose connected');
+    }});
+
+module.exports = mongoose;
